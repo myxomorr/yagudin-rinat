af1bd502d40ff33b58d4ab4be619cdae798999eb
myxomor <yagudin.rinat2005@mail.ru>
2024-10-01 21:58:36 +0300

    build.sh for all files

---
+++ week2/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd task1/task1.1
+./build.sh
+cd ../task1.2/
+./build.sh
+cd ../../task2/task2.1
+./build.sh
+cd ../task2.2
+./build.sh
+cd ../../task3/task3.1
+./build.sh
+cd ../task3.2
+./build.sh
