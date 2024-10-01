5dcb66359d1bded5387cf543b643742384bc0807
myxomor <yagudin.rinat2005@mail.ru>
2024-10-01 19:50:29 +0300

    +task 2.2

--- week2/task2/task2.2/src/main.cpp
+++ week2/task2/task2.2/src/main.cpp
@@ -4,16 +4,16 @@
 int main() {
     BankAccount account("1111");
     
-    account.deposit(10000);
+    account.deposit(100);
     cout << "Баланс: " << account.getBalance() << endl;
 
-    if (account.withdraw(5500)) {
+    if (account.withdraw(50)) {
         cout << "Снято 550. Баланс: " << account.getBalance() << endl;
     } else {
         cout << "Недостаточно средств для снятия." << endl;
     }
 
-    if (account.withdraw(200)) {
+    if (account.withdraw(60)) {
         cout << "Снято 600. Баланс: " << account.getBalance() << endl;
     } else {
         cout << "Недостаточно средств для снятия." << endl;
